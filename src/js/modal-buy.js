e96790cc63f2d79e99bf46866b58f195bfa75cb0
VladaBoiko <102071674+VladaBoiko@users.noreply.github.com>
2023-04-12 17:54:06 +0300

    ready

--- src/js/modal-buy.js
+++ src/js/modal-buy.js
@@ -1,14 +1,16 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[buy-modal-open]"),
-    closeModalBtn: document.querySelector("[buy-modal-close]"),
-    modal: document.querySelector("[buy-data-modal]"),
+    openModalBtn: document.querySelector('[buy-modal-open]'),
+    openModalBtnSellers: document.querySelector('[buy-modal-open-sellers]'),
+    closeModalBtn: document.querySelector('[buy-modal-close]'),
+    modal: document.querySelector('[buy-data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnSellers.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
